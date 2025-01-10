2a77c0e052940c180d0b2a6fa67f87d149553b0f
Hollie Jaques <holliejaques@gmail.com>
2025-01-10 23:07:55 +0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  root "lists#index"
 
   # Defines the root path route ("/")
   # root "posts#index"
@@ -18,7 +19,7 @@ Rails.application.routes.draw do
   # delete
 
   # create a block so we can nest bookmarks under lists - why there is a do
-  resources :lists do
+  resources :lists, except: :index do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
