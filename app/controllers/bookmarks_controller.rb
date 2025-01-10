1f176675ddf326a083f411e47a61ee903fea1d96
Hollie Jaques <holliejaques@gmail.com>
2025-01-10 22:30:15 +0700

    added scss styling

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -18,9 +18,9 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:bookmark_id])
+    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to lists_path, status: see_other
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
