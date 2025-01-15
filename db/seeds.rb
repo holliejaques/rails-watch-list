5408ecfa4667dd9ca3c004877208f97c3e0bf1b8
Hollie Jaques <holliejaques@gmail.com>
2025-01-15 22:44:04 +0700

    Add dotenv-rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,40 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-require 'faker'
+# require 'faker'
+require 'open-uri' # reads an api
+require 'json' # formats everything correctly
 
+puts 'Cleaning up database...'
 Movie.destroy_all
+puts 'Database cleaned'
 
-20.times do
-  Movie.create(
-    title: Faker::Movie.title,
-    overview: Faker::Lorem.paragraphs,
-    poster_url: Faker::LoremFlickr.image,
-    rating: Faker::Number.between(from: 1, to: 5)
-  )
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+
+12.times do |i|
+  puts "Importing movies from page #{i + 1}"
+  movies = JSON.parse(URI.open("#{url}?page=#{i + 1}").read)['results']
+  movies.each do |movie|
+    puts "Creating #{movie['title']}"
+    base_poster_url = 'https://image.tmdb.org/t/p/original'
+    Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
+      rating: movie['vote_average']
+    )
+  end
 end
+
+puts "#{Movie.count} movies created"
+
+# Movie.destroy_all
+
+# 20.times do
+#   Movie.create(
+#     title: Faker::Movie.title,
+#     overview: Faker::Lorem.paragraphs,
+#     poster_url: Faker::LoremFlickr.image,
+#     rating: Faker::Number.between(from: 1, to: 5)
+#   )
+# end
